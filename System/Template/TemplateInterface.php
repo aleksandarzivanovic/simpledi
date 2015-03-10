66e27a000436659a5a018d3ba32b00d9c1ffbc98
Aleksandar Zivanovic <aleksandar.zivanovic@boutsourcing.com>
2015-03-10 10:36:48 +0100

    template engine interface documented

--- System/Template/TemplateInterface.php
+++ System/Template/TemplateInterface.php
@@ -4,10 +4,35 @@ namespace System\Template;
 
 interface TemplateInterface
 {
+    /**
+     * @param array $areas
+     */
     public function setAreas(array $areas);
+
+    /**
+     * @param array $variables
+     */
     public function setVariables(array $variables);
+
+    /**
+     * @param string $area
+     * @param string $value
+     */
     public function setArea($area, $value);
+
+    /**
+     * @param string $area
+     * @return null|array
+     */
     public function getArea($area);
+
+    /**
+     * @return array
+     */
     public function getAreas();
+
+    /**
+     * @return array
+     */
     public function getVariables();
 }
