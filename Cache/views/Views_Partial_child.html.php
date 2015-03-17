cc440f2af6a5015d02186146875a3d5fc4ce7e89
Aleksandar Zivanovic <aleksandar.zivanovic@boutsourcing.com>
2015-03-17 14:32:13 +0100

    refresh template cache if source is modified

---
+++ Cache/views/Views_Partial_child.html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>ViBE - PHP Framework</title>
+</head>
+<body>
+    
+    <?php foreach ($array as $value) { ?>
+    
+        <p>
+            Type: <em><?php echo $value["type"];?></em><br />
+            Position: <em><?php echo $value["position"];?></em><br />
+        </p>
+    
+<?php } ?>
+
+</body>
+</html>
